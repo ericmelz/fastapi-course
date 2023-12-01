605ebd443a3466fc41843ee1893e00c066e77ca3
ericmelz <eric@emelz.com>
2023-12-01 07:34:18 -0800

    added books

---
+++ mine/Project1/books.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': "science"},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': "science"},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': "history"},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': "math"},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': "math"},
+]
+
+@app.get('/api-endpoint')
+async def first_api():
+    return {'message': 'Hello Eric!'}
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
