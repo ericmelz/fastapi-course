0f871e4e8069f93ad3dc1a67ec5dd1b460b38d4a
ericmelz <eric@emelz.com>
2023-12-01 08:29:20 -0800

    removed a method

--- mine/Project1/books.py
+++ mine/Project1/books.py
@@ -26,11 +26,6 @@ async def read_all_books():
     return {'book_title': 'My  favorite book!'}
 
 
-@app.get("/books/{dynamic_param}")
-async def read_all_books(dynamic_param: str):
-    return {'dynamic_param': dynamic_param}
-
-
 @app.get('/books/{book_title}')
 async def read_book(book_title: str):
     for book in BOOKS:
