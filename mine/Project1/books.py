439c39cb670612c6bf93338a46542c69d58af62f
ericmelz <eric@emelz.com>
2023-12-02 08:28:03 -0800

    added put and delete

--- mine/Project1/books.py
+++ mine/Project1/books.py
@@ -53,6 +53,21 @@ async def read_category_by_query(book_author: str, category: str):
     return books_to_return
 
 
-@app.post('books/create_book')
+@app.post('/books/create_book')
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
