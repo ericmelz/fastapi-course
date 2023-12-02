2b2019b5cc506de8d46b6d98fb5f52ebd2414a83
ericmelz <eric@emelz.com>
2023-12-02 07:43:19 -0800

    work

--- mine/Project1/books.py
+++ mine/Project1/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -31,3 +31,28 @@ async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get('/books/{book_author}/')
+async def read_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post('books/create_book')
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
